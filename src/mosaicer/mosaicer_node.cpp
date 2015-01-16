9a92583195a89147c8710c1109ec5c079cdf38d0
Gareth Cross <gcross.code@icloud.com>
2015-01-16 18:24:39 -0500

    Added launch file

--- src/mosaicer/mosaicer_node.cpp
+++ src/mosaicer/mosaicer_node.cpp
@@ -15,16 +15,18 @@ Node::Node(const ros::NodeHandle &nh) : pnh_(nh) {
   
   sub_image_.subscribe(*it_, "image", kROSQueueSize);
   sub_info_.subscribe(pnh_, "camera_info", kROSQueueSize);
+  sub_homo_.subscribe(pnh_, "homography", kROSQueueSize);
   
   sync_ = make_shared<message_filters::Synchronizer<SyncPolicy>>(
-              SyncPolicy(kROSQueueSize), sub_image_, sub_info_);
-  sync_->registerCallback(boost::bind(&Node::callback,this, _1, _2));
+              SyncPolicy(kROSQueueSize), sub_image_, sub_info_, sub_homo_);
+  sync_->registerCallback(boost::bind(&Node::callback,this, _1, _2, _3));
   
   pub_image_ = pnh_.advertise<sensor_msgs::Image>("mosaic_image", 1);
 }
 
 void Node::callback(const sensor_msgs::ImageConstPtr& image,
-                    const sensor_msgs::CameraInfoConstPtr& info) {
+                    const sensor_msgs::CameraInfoConstPtr& info, 
+                    const stitchtron9000::HomographyConstPtr& homo) {
   
   cv_bridge::CvImageConstPtr bridged = cv_bridge::toCvShare(image,"mono8");
   if (!bridged || bridged->image.empty()) {
