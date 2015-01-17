b97081691ab981a477ad95fc78d62f7c08cf880c
Cedric de Crousaz <cedricd@ethz.ch>
2015-01-17 16:38:50 -0500

    Fixed merge

--- src/homographer/homographer_node.cpp
+++ src/homographer/homographer_node.cpp
@@ -15,81 +15,9 @@ static std::vector<int> id_prev;  // for indices of current keyframe
 static ros::Subscriber sub_keyframe;
 static ros::Publisher pub_homography;
 
+void print_homography(const stitchtron9000::Homography &homography); 
+
 // Callback for KeyFrame
-void keyframe_cb( const stitchtron9000::KeyFrame& msg ) {
-
-	ROS_INFO("New keyframe received");
-	hom_curr.header = msg.header;
-	
-	std::vector<cv::Point2f> points_curr;		// for coordinates of current keyframe
-	std::vector<int> id_curr;					// for indices of current keyframe
-	std::vector<cv::Point2f> tracked_pts_prev;
-	std::vector<cv::Point2f> tracked_pts_curr;
-
-	// 1) copy all features	
-	for(auto & fet:msg.features) {
-		points_curr.push_back(cv::Point2f(fet.x,fet.y));	// constructing feature vector
-		id_curr.push_back(fet.id);							// constructing image index vector
-	}
-
-	if (id_prev.empty()) {
-		ROS_INFO("First image received");
-		
-		// Return identity matrix
-		hom_curr.homography[0] = hom_curr.homography[4] = hom_curr.homography[8] = 1;
-
-		std::cout << "Initial homography matrix: " << std::endl;
-	}
-	else {
-		ROS_INFO("New points copied");
-		// 2) now fill two new vectors with tracked points
-		//    since we constructed the tracked vector as intersection, 
-		//	  the element will certainly be present (not rechecking)
-		for (auto & fet:msg.tracked_ids) {
-			// find in previous points
-			int pos_prev = std::find(id_prev.begin(),id_prev.end(),fet) - id_prev.begin();
-			tracked_pts_prev.push_back(points_prev[pos_prev]);
-
-			// find in current (new) points
-			int pos_curr = std::find(id_curr.begin(),id_curr.end(),fet) - id_curr.begin();
-			tracked_pts_curr.push_back(points_curr[pos_curr]);
-		}
-		ROS_INFO("Common elements extracted");
-
-		// 4) compute fundamental matrix and prune point vectors accordingly
-		std::vector<uchar> status;
-		cv::findFundamentalMat(tracked_pts_prev,tracked_pts_curr,CV_FM_RANSAC,3,0.99,status);
-
-		int ind = 0;
-		for (auto & it:status) {
-			if (!it) { // if outlier, kick from vector
-				tracked_pts_prev.erase(tracked_pts_prev.begin()+ind);
-				tracked_pts_curr.erase(tracked_pts_curr.begin()+ind);
-			}
-			else {
-				++ind;
-			}
-		}
-
-		// 5) extract homography matrix
-		cv::Mat homography_mat = cv::findHomography(tracked_pts_prev,tracked_pts_curr);
-
-		// copy matrix into c++ container and publish
-		for (int i=0; i<9; ++i) {
-			hom_curr.homography[i] = homography_mat.data[i];
-		};
-
-	}
-	
-	// publish homography and update previous points	
-	pub_homography.publish(hom_curr);
-	std::vector<cv::Point2f> points_prev = points_curr;
-	std::vector<int> id_prev = id_curr;
-}
-
-
-int main (int argc, char **argv) {
-
 void keyframe_cb(const stitchtron9000::KeyFrame& msg) {
   ROS_INFO("New keyframe received");
   hom_curr.header = msg.header;
